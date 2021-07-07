a382fac96b46f6a35ee74f593bdaf7d77522ebec
Arseny <44338288+parhimarsen@users.noreply.github.com>
2021-07-07 19:31:24 +0300

    Create Database.
    Resolve connections between entities.

--- CinemaProject.WebAPI/CinemaProject.WebAPI/Startup.cs
+++ CinemaProject.WebAPI/CinemaProject.WebAPI/Startup.cs
@@ -1,15 +1,10 @@
+using CinemaProject.DAL.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CinemaProject.WebAPI
 {
@@ -25,6 +20,12 @@ namespace CinemaProject.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("CinemaConnection");
+
+            using (CinemaContext db = new CinemaContext())
+            {
+                db.Database.EnsureCreated();
+            }
 
             services.AddControllers();
         }
