9a6cbcd082359ff86323119be4294b5dcadc4b7c
Arseny <44338288+parhimarsen@users.noreply.github.com>
2021-07-08 13:35:39 +0300

    Create GenericRepository и UnitOfWork

---
+++ CinemaProject.WebAPI/CinemaProject.DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace CinemaProject.DAL.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetAsync(Guid id);
+        IEnumerable<T> GetWithInclude(params Expression<Func<T, object>>[] includeProperties);
+        Task<T> InsertAsync(T item);
+        Task RemoveAsync(Guid id);
+        Task UpdateAsync(Guid id);
+        Task<bool> ExistsAsync(Guid id);
+    }
+}
---
+++ CinemaProject.WebAPI/CinemaProject.DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,79 @@
+﻿using CinemaProject.DAL.Contexts;
+using CinemaProject.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace CinemaProject.DAL.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly CinemaContext _context;
+
+        public GenericRepository(CinemaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetAsync(Guid id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public IEnumerable<T> GetWithInclude(params Expression<Func<T, object>>[] includeProperties)
+        {
+            return Include(includeProperties).ToList();
+        }
+
+        public async Task<T> InsertAsync(T item)
+        {
+            await _context.Set<T>().AddAsync(item);
+
+            return item;
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            T item = await GetAsync(id);
+
+            _context.Set<T>().Remove(item);
+        }
+
+        public async Task UpdateAsync(Guid id)
+        {
+            T item = await GetAsync(id);
+
+            if (item != null)
+            {
+                _context.Entry(item).State = EntityState.Detached;
+            }
+        }
+
+        public async Task<bool> ExistsAsync(Guid id)
+        {
+            T item = await GetAsync(id);
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private IQueryable<T> Include(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            return includeProperties
+                .Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+        }
+    }
+}
