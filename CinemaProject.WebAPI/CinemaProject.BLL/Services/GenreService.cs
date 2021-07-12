7b19f09928c758953d6e5a99962e8d4592acaec8
Arseny <44338288+parhimarsen@users.noreply.github.com>
2021-07-12 10:16:45 +0300

    Create GenreService

---
+++ CinemaProject.WebAPI/CinemaProject.BLL/Services/GenreService.cs
@@ -0,0 +1,61 @@
+﻿using CinemaProject.BLL.Mappers;
+using CinemaProject.BLL.Models;
+using CinemaProject.DAL.Entities;
+using CinemaProject.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaProject.BLL.Services
+{
+    public class GenreService
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public GenreService(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Genre[]> GetAllAsync()
+        {
+            IEnumerable<GenreEntity> genreEntities = await _unitOfWork.GenresRepository.GetAllAsync();
+
+            return genreEntities
+                .Select(genre => genre.ToModel())
+                .ToArray();
+        }
+
+        public async Task<Genre> InsertAsync(Genre genre)
+        {
+            GenreEntity genreEntity = new GenreEntity
+            {
+                Id = Guid.NewGuid(),
+                Name = genre.Name,
+            };
+
+            await _unitOfWork.GenresRepository.InsertAsync(genreEntity);
+            await _unitOfWork.SaveAsync();
+
+            return genreEntity.ToModel();
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            IEnumerable<FilmGenreEntity> filmGenreEntities = await _unitOfWork.FilmGenresRepository.GetAllAsync();
+
+            FilmGenreEntity[] filmGenres = filmGenreEntities
+                .Where(genre => genre.GenreId == id)
+                .ToArray();
+
+            foreach (var filmGenre in filmGenres)
+            {
+                await _unitOfWork.FilmGenresRepository.RemoveAsync(filmGenre.FilmId, filmGenre.GenreId);
+            }
+
+            await _unitOfWork.ActorsRepository.RemoveAsync(id);
+            await _unitOfWork.SaveAsync();
+        }
+    }
+}
---
+++ CinemaProject.WebAPI/CinemaProject.DAL/Repositories/FilmGenresRepository.cs
@@ -0,0 +1,55 @@
+﻿using CinemaProject.DAL.Contexts;
+using CinemaProject.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CinemaProject.DAL.Repositories
+{
+    public class FilmGenresRepository
+    {
+        private readonly CinemaContext _context;
+
+        public FilmGenresRepository(CinemaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<FilmGenreEntity>> GetAllAsync()
+        {
+            return await _context.Set<FilmGenreEntity>().ToListAsync();
+        }
+
+        public async Task<FilmGenreEntity> GetAsync(Guid filmId, Guid genreId)
+        {
+            return await _context.Set<FilmGenreEntity>().FindAsync(filmId, genreId);
+        }
+
+        public async Task<FilmGenreEntity> InsertAsync(FilmGenreEntity filmGenreEntity)
+        {
+            await _context.Set<FilmGenreEntity>().AddAsync(filmGenreEntity);
+
+            return filmGenreEntity;
+        }
+
+        public async Task RemoveAsync(Guid filmId, Guid genreId)
+        {
+            FilmGenreEntity filmGenreEntity = await GetAsync(filmId, genreId);
+
+            _context.Set<FilmGenreEntity>().Remove(filmGenreEntity);
+        }
+
+        public async Task<bool> ExistsAsync(FilmGenreEntity filmGenreEntity)
+        {
+            FilmGenreEntity filmGenre = await GetAsync(filmGenreEntity.FilmId, filmGenreEntity.GenreId);
+
+            if (filmGenre == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
