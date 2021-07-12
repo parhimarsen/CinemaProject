71bcda0bc63ddbbabc07f5f8d1e02517067416ea
Arseny <44338288+parhimarsen@users.noreply.github.com>
2021-07-12 09:52:06 +0300

    Create Mapper

--- CinemaProject.WebAPI/CinemaProject.BLL/Mappers/ModelMappingExtensions.cs
+++ CinemaProject.WebAPI/CinemaProject.BLL/Mappers/ModelMappingExtensions.cs
@@ -1,6 +1,6 @@
 ﻿using CinemaProject.BLL.Models;
 using CinemaProject.DAL.Entities;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace CinemaProject.BLL.Mappers
 {
@@ -8,6 +8,13 @@ namespace CinemaProject.BLL.Mappers
     {
         public static User ToModel(this UserEntity @this)
         {
+            List<Ticket> Tickets = new List<Ticket>();
+
+            foreach (TicketEntity ticketEntity in @this.Tickets)
+            {
+                Tickets.Add(ticketEntity.ToModel());
+            }
+
             return new User
             {
                 Id = @this.Id,
@@ -15,6 +22,7 @@ namespace CinemaProject.BLL.Mappers
                 Login = @this.Login,
                 Password = @this.Password,
                 IsAdmin = @this.IsAdmin,
+                Tickets = Tickets.ToArray()
             };
         }
 
@@ -23,7 +31,10 @@ namespace CinemaProject.BLL.Mappers
             return new Ticket
             {
                 Id = @this.Id,
-                Status = @this.Status
+                Status = @this.Status,
+                UserId = @this.UserId,
+                SessionId = @this.SessionId,
+                SeatId = @this.SeatId
             };
         }
 
@@ -34,6 +45,100 @@ namespace CinemaProject.BLL.Mappers
                 Id = @this.Id,
                 Name = @this.Name,
                 Cost = @this.Cost
+            };
+        }
+
+        public static Film ToModel(this FilmEntity @this)
+        {
+            return new Film
+            {
+                Id = @this.Id,
+                Name = @this.Name,
+                Director = @this.Director,
+                ReleaseDate = @this.ReleaseDate,
+                Country = @this.Country,
+                Duration = @this.Duration
+            };
+        }
+
+        public static Actor ToModel(this ActorEntity @this)
+        {
+            return new Actor
+            {
+                Id = @this.Id,
+                Name = @this.Name
+            };
+        }
+
+        public static Genre ToModel(this GenreEntity @this)
+        {
+            return new Genre
+            {
+                Id = @this.Id,
+                Name = @this.Name
+            };
+        }
+
+        public static Session ToModel(this SessionEntity @this)
+        {
+            return new Session
+            {
+                Id = @this.Id,
+                ShowStart = @this.ShowStart,
+                ShowEnd = @this.ShowEnd,
+                Cost = @this.Cost,
+                HallId = @this.HallId,
+                FilmId = @this.FilmId
+            };
+        }
+        public static Hall ToModel(this HallEntity @this)
+        {
+            return new Hall
+            {
+                Id = @this.Id,
+                Name = @this.Name,
+                CinemaId = @this.CinemaId,
+            };
+        }
+
+        public static Seat ToModel(this SeatEntity @this)
+        {
+            return new Seat
+            {
+                Id = @this.Id,
+                NumberOfSeat = @this.NumberOfSeat,
+                Row = @this.Row,
+                Column = @this.Column,
+                HallId = @this.HallId,
+                TypeOfSeatId = @this.TypeOfSeatId
+            };
+        }
+
+        public static TypeOfSeat ToModel(this TypeOfSeatEntity @this)
+        {
+            return new TypeOfSeat
+            {
+                Id = @this.Id,
+                Name = @this.Name,
+                ExtraPaymentPercent = @this.ExtraPaymentPercent
+            };
+        }
+
+        public static City ToModel(this CityEntity @this)
+        {
+            return new City
+            {
+                Id = @this.Id,
+                Name = @this.Name
+            };
+        }
+
+        public static Cinema ToModel(this CinemaEntity @this)
+        {
+            return new Cinema
+            {
+                Id = @this.Id,
+                Name = @this.Name
             };
         }
     }
