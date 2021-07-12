5a1619bea020b7e32dd4032b99b84b5697121bfd
Arseny <44338288+parhimarsen@users.noreply.github.com>
2021-07-12 10:16:16 +0300

    Create ActorService

---
+++ CinemaProject.WebAPI/CinemaProject.BLL/Services/ActorService.cs
@@ -0,0 +1,61 @@
+﻿using CinemaProject.BLL.Mappers;
+using CinemaProject.BLL.Models;
+using CinemaProject.DAL.Entities;
+using CinemaProject.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaProject.BLL.Services
+{
+    public class ActorService
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public ActorService(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Actor[]> GetAllAsync()
+        {
+            IEnumerable<ActorEntity> actorEntities = await _unitOfWork.ActorsRepository.GetAllAsync();
+
+            return actorEntities
+                .Select(actor => actor.ToModel())
+                .ToArray();
+        }
+
+        public async Task<Actor> InsertAsync(Actor actor)
+        {
+            ActorEntity actorEntity = new ActorEntity
+            {
+                Id = Guid.NewGuid(),
+                Name = actor.Name,
+            };
+
+            await _unitOfWork.ActorsRepository.InsertAsync(actorEntity);
+            await _unitOfWork.SaveAsync();
+
+            return actorEntity.ToModel();
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            IEnumerable<CastEntity> castEntities = await _unitOfWork.CastsRepository.GetAllAsync();
+
+            CastEntity[] casts = castEntities
+                .Where(cast => cast.ActorId == id)
+                .ToArray();
+
+            foreach (var cast in casts)
+            {
+                await _unitOfWork.CastsRepository.RemoveAsync(cast.FilmId, cast.ActorId);
+            }
+
+            await _unitOfWork.ActorsRepository.RemoveAsync(id);
+            await _unitOfWork.SaveAsync();
+        }
+    }
+}
