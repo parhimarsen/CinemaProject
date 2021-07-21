520ec38e40d30c243b0d076111e815f8feed1ad9
Arseny <44338288+parhimarsen@users.noreply.github.com>
2021-07-21 12:56:02 +0300

    Create TypesOfSeatController

---
+++ CinemaProject.WebAPI/CinemaProject.WebAPI/Controllers/TypesOfSeatController.cs
@@ -0,0 +1,58 @@
+﻿using CinemaProject.BLL.Models;
+using CinemaProject.BLL.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CinemaProject.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TypesOfSeatController : ControllerBase
+    {
+        private readonly TypeOfSeatService _typeOfSeatService;
+
+        public TypesOfSeatController(TypeOfSeatService typeOfSeatService)
+        {
+            _typeOfSeatService = typeOfSeatService;
+        }
+
+        // GET: api/TypesOfSeat
+        [HttpGet]
+        public IActionResult GetTypesOfSeat()
+        {
+            List<TypeOfSeat> response =  _typeOfSeatService.GetAllAsync();
+
+            return Ok(response);
+        }
+
+        // POST: api/TypesOfSeat
+        [HttpPost]
+        public async Task<IActionResult> PostTypeOfSeat(TypeOfSeat model)
+        {
+            TypeOfSeat response = await _typeOfSeatService.InsertAsync(model);
+
+            return Ok(response);
+        }
+
+
+        // PUT: api/TypesOfSeat
+        [HttpPut]
+        public async Task<IActionResult> PutTypeOfSeat(TypeOfSeat model)
+        {
+            await _typeOfSeatService.UpdateAsync(model);
+
+            return NoContent();
+        }
+
+        // DELETE: api/TypesOfSeat/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTypeOfSeat(Guid id)
+        {
+            await _typeOfSeatService.RemoveAsync(id);
+
+            return NoContent();
+        }
+    }
+}
