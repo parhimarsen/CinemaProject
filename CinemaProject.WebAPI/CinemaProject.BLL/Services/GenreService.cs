3bad07f35fa162322b45235dfccf905b7291356f
Arseny <44338288+parhimarsen@users.noreply.github.com>
2021-07-21 12:52:12 +0300

    Create GenresController

--- CinemaProject.WebAPI/CinemaProject.BLL/Services/GenreService.cs
+++ CinemaProject.WebAPI/CinemaProject.BLL/Services/GenreService.cs
@@ -3,6 +3,7 @@ using CinemaProject.BLL.Models;
 using CinemaProject.DAL.Entities;
 using CinemaProject.DAL.Repositories;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Z.EntityFramework.Plus;
@@ -18,10 +19,37 @@ namespace CinemaProject.BLL.Services
             _unitOfWork = unitOfWork;
         }
 
-        public IQueryable<Genre> GetAllAsync()
+        public IQueryable<Genre> GetAll()
         {
             return _unitOfWork.GenresRepository.GetAll()
-                .Select(genre => genre.ToModel());
+                .Select(genre => new Genre
+                {
+                    Id = genre.Id,
+                    Name = genre.Name
+                });
+        }
+
+        public async Task<Genre[]> GetAllOfFilm(Guid filmId)
+        {
+            if (!await _unitOfWork.FilmsRepository.ExistsAsync(filmId))
+            {
+                return null;
+            }
+
+            IList<Genre> genresOfFilm = new List<Genre>();
+
+            IQueryable<FilmGenreEntity> filmGenreQuery = _unitOfWork.FilmGenresRepository.GetAll();
+
+            IEnumerable<GenreEntity> genres = filmGenreQuery
+                .Where(x => x.FilmId == filmId)
+                .Select(x => x.Genre);
+
+            foreach (var genre in genres)
+            {
+                genresOfFilm.Add(genre.ToModel());
+            }
+
+            return genresOfFilm.ToArray();
         }
 
         public async Task<Genre> InsertAsync(Genre genre)
@@ -47,6 +75,21 @@ namespace CinemaProject.BLL.Services
                 .Delete();
 
             await _unitOfWork.GenresRepository.RemoveAsync(id);
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task UpdateAsync(Genre genre)
+        {
+            if (!await _unitOfWork.GenresRepository.ExistsAsync(genre.Id))
+            {
+                return;
+            }
+
+            GenreEntity genreEntity = await _unitOfWork.GenresRepository.GetAsync(genre.Id);
+
+            genreEntity.Name = genre.Name;
+
+            await _unitOfWork.GenresRepository.UpdateAsync(genreEntity.Id);
             await _unitOfWork.SaveAsync();
         }
     }
---
+++ CinemaProject.WebAPI/CinemaProject.WebAPI/Controllers/GenresController.cs
@@ -0,0 +1,57 @@
+﻿using CinemaProject.BLL.Models;
+using CinemaProject.BLL.Services;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaProject.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly GenreService _genreService;
+
+        public GenresController(GenreService genreService)
+        {
+            _genreService = genreService;
+        }
+
+        // GET: api/Genres
+        [HttpGet]
+        [EnableQuery]
+        public IQueryable<Genre> GetGenres()
+        {
+            return _genreService.GetAll();
+        }
+
+        // POST: api/Genres
+        [HttpPost]
+        public async Task<IActionResult> PostGenre(Genre model)
+        {
+            Genre response = await _genreService.InsertAsync(model);
+
+            return Ok(response);
+        }
+
+        // PUT: api/Genres
+        [HttpPut]
+        public async Task<IActionResult> PutGenre(Genre model)
+        {
+            await _genreService.UpdateAsync(model);
+
+            return NoContent();
+        }
+
+        // DELETE: api/Genres/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGenre(Guid id)
+        {
+            await _genreService.RemoveAsync(id);
+
+            return NoContent();
+        }
+    }
+}
